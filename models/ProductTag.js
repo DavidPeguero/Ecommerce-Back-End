f7ce009313515c0e66affb6eba3684b25bd149a4
David Peguero <david.peguero.cancel@gmail.com>
2023-05-02 16:50:09 -0400

    Added all the columns to the product model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id : {
+      type : DataTypes.INTEGER,
+      references: {
+        // This references the `driver` model, which we set in `Driver.js` as its `modelName` property
+        model: 'driver',
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -4,10 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// // Define a Driver as having one License to create a foreign key in the `license` table
+// Driver.hasOne(License, {
+//   foreignKey: 'driver_id',
+//   // When we delete a Driver, make sure to also delete the associated License.
+
+//   onDelete: 'CASCADE',
+// });
+
+// // We can also define the association starting with License
+// License.belongsTo(Driver, {
+//   foreignKey: 'driver_id',
+// });
+
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey : 'id',
+  onDelete : 'CASCADE'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  through : 'id'
+})
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
