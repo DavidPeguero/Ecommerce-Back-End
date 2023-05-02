af5ab7771aeba11d4e1987fa8c27f3b35543be6f
David Peguero <david.peguero.cancel@gmail.com>
2023-05-02 17:47:41 -0400

    Fixed associations

--- models/index.js
+++ models/index.js
@@ -4,36 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// // Define a Driver as having one License to create a foreign key in the `license` table
-// Driver.hasOne(License, {
-//   foreignKey: 'driver_id',
-//   // When we delete a Driver, make sure to also delete the associated License.
-
-//   onDelete: 'CASCADE',
-// });
-
-// // We can also define the association starting with License
-// License.belongsTo(Driver, {
-//   foreignKey: 'driver_id',
-// });
-
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey : 'id',
-  onDelete : 'CASCADE'
-})
+Product.belongsTo(Category, {})
 // Categories have many Products
 Category.hasMany(Product, {
-  through : 'id'
+  foreignKey : 'category_id',
+  onDelete : 'CASCADE'
 })
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through : 'id'
+  through : ProductTag,
+  as: 'productTag-tag',
+  foreignKey : 'product_id'
 })
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through : 'id'
+  through : ProductTag,
+  as: 'productTag-tag',
+  foreignKey : 'tag_id'
 })
+
 module.exports = {
   Product,
   Category,
