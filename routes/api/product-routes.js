54fa762f218a397a1e257774126821270c41125f
David Peguero <david.peguero.cancel@gmail.com>
2023-05-03 14:40:22 -0400

    Added get routes to tag-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,8 +23,12 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag }],
+      include: [{model: Category}, {model:Tag, through : ProductTag, as : 'product_tags'}],
     });
+    if(!productData){
+      res.status(404).json({error: 404, message :`Could not find product with ID: ${req.params.id}`});
+      return;
+    }
     res.status(200).json(productData);
   }
   catch (err) {
